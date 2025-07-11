894faa69e2d8b47b52b694088fa4fa1791097bdf
Wellington Santos <weltossousa@gmail.com>
2025-07-11 16:28:34 -0300

    feat: add embedding answers response with gemini

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -2,7 +2,8 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
 import z from "zod/v4";
-import { eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
+import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts";
 
 export const createQuestionRoute: FastifyPluginAsyncZod = async (app) => {
   app.post(
@@ -30,18 +31,48 @@ export const createQuestionRoute: FastifyPluginAsyncZod = async (app) => {
         throw new Error("Room not exists");
       }
 
+      const embeddings = await generateEmbeddings(question);
+
+      const embeddingsAsString = `[${embeddings.join(",")}]`;
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3);
+
+      let answer: string | null = null;
+
+      if (chunks.length > 0) {
+        const transcription = chunks.map((chunk) => chunk.transcription);
+
+        answer = await generateAnswer(question, transcription);
+      }
+
       const result = await db
         .insert(schema.questions)
-        .values({ question, roomId })
+        .values({ question, roomId, answer })
         .returning();
 
       const insertQuestion = result[0];
 
-      if (!result[0]) {
+      if (!insertQuestion) {
         throw new Error("Failed to create new question.");
       }
 
-      return reply.status(201).send({ questionId: insertQuestion.id });
+      return reply.status(201).send({ questionId: insertQuestion.id, answer });
     }
   );
 };
